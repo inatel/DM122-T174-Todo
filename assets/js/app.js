579710744be5f37dfb0ea03f140b355d881b0641
Edy Segura <edysegura@gmail.com>
2020-09-26 16:39:32 -0300

    Fix manifest and sw.js path

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,7 +19,7 @@ class App {
       const onfailure = () => console.log('[Service Worker] Failed');
 
       navigator.serviceWorker
-        .register('sw.js')
+        .register('../../sw.js')
         .then(onsuccess)
         .catch(onfailure);
     }
